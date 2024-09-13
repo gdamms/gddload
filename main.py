ee8e8e39005fee18c15a03b713d9ecdc72ef8434
gdamms <damguillotin@gmail.com>
2024-09-13 16:34:33 +0200

    huge refactor, argument parsing and comments

--- main.py
+++ main.py
@@ -3,8 +3,67 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import os
-import io
 import hashlib
+import argparse
+
+
+class FileStatus:
+    PENDING = 0
+    DOWNLOADING = 1
+    DOWNLOADED = 2
+    ALREADY_DOWNLOADED = 3
+    CORRUPTED = 4
+    WARNING = 5
+
+
+class FileType:
+    UNKNOWN = -1
+    FILE = 0
+    FOLDER = 1
+
+
+class File:
+    def __init__(self, id: str):
+        self.id = id
+        self.name = ''
+        self.size = 0
+        self.type = FileType.UNKNOWN
+        self.sha256 = ''
+        self.progress = 0.0
+        self.status = FileStatus.PENDING
+        self.children = []
+
+    @property
+    def formatted_size(self):
+        return format_size(self.size)
+
+    @property
+    def formatted_progress(self):
+        return format_progress(self.progress)
+
+
+def parse_args() -> None:
+    """Parse the command line arguments."""
+
+    parser = argparse.ArgumentParser(description='Download files from Google Drive')
+    parser.add_argument('file_id', type=str, help='The Google Drive file ID')
+    parser.add_argument('--save_path', type=str, default='.', help='The path to save the files')
+    parser.add_argument('--check', action='store_true', help='Check the sha256 of the files')
+    parser.add_argument('--overwrite', action='store_true',
+                        help='Overwrite the file if it already exists but does not match the sha256')
+    parser.add_argument('--force', action='store_true', help='Force the download of the file even if it already exists')
+    parser.add_argument('--retry', type=int, default=1,
+                        help='The number of retries in case of error. (implies --check)')
+    args = parser.parse_args()
+
+    global config
+    config = {}
+    config['file_id'] = args.file_id
+    config['save_path'] = args.save_path
+    config['check'] = args.check
+    config['overwrite'] = args.overwrite
+    config['force'] = args.force
+    config['retry'] = args.retry
 
 
 creds = service_account.Credentials.from_service_account_file(
@@ -12,222 +71,240 @@ creds = service_account.Credentials.from_service_account_file(
     scopes=['https://www.googleapis.com/auth/drive']
 )
 service = build("drive", "v3", credentials=creds)
-root_info = None
+root_file = None
 
+ANSI_COLORS = {
+    'pending': '\033[96m',
+    'downloading': '\033[94m',
+    'downloaded': '\033[92m',
+    'done': '\033[92m',
+    'corrupted': '\033[91m',
+    'default': '\033[0m',
+    'warning': '\033[93m',
+}
+BAR_LENGTH = 4
 
-def update_progress(info):
-    if info['type'] == 'folder':
-        for child in info['children']:
+status = 'idle'
+
+def update_progress(file: File) -> None:
+    """Update the progress of the file.
+
+    Args:
+        file (File): The file to update.
+    """
+    if file.type == FileType.FOLDER:
+        warning = False
+        for child in file.children:
             update_progress(child)
-        if info['size'] == 0:
-            info['progress'] = 1
+            if child.status == FileStatus.CORRUPTED or child.status == FileStatus.WARNING:
+                warning = True
+        if file.size == 0:
+            file.progress = 1
         else:
-            info['progress'] = sum([child['progress'] * child['size'] for child in info['children']]) / info['size']
+            file.progress = sum([child.progress * child.size for child in file.children]) / file.size
+        if warning:
+            file.status = FileStatus.WARNING
 
 
 def update_root_progress():
-    global root_info
-    update_progress(root_info)
-
-def download_folder(info):
-    if not os.path.exists(info['path']):
-        os.makedirs(info['path'])
-    for child in info['children']:
-        download_file_recursive(child)
-    info['status'] = 'downloaded'
+    global root_file
+    if not root_file:
+        # TODO: better error handling
+        raise Exception('Root file is not set')
+    update_progress(root_file)
 
 
-def download_file(info):
+def download_folder(file: File, save_path: str) -> None:
+    """Download a folder from Google Drive.
+
+    Args:
+        file (File): The file to download.
+        save_path (str): The path to save the folder.
+    """
+    folder_path = os.path.join(save_path, file.name)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    for child in file.children:
+        download_file_recursive(child, folder_path)
+    file.status = FileStatus.DOWNLOADED
+
+
+def download_file(file: File, save_path: str) -> None:
+    """Download a file from Google Drive.
+
+    Args:
+        file (File): The file to download.
+        save_path (str): The path to save the file.
+    """
     global service
-    if os.path.exists(info['path']):
-        with open(info['path'], 'rb') as f:
+
+    file_path = os.path.join(save_path, file.name)
+
+    if os.path.exists(file_path):
+        # check file integrity
+        # TODO: is check needed?
+        with open(file_path, 'rb') as f:
             sha256 = hashlib.sha256(f.read()).hexdigest()
-        info['progress'] = 1
-        if sha256 == info['sha256']:
-            info['status'] = 'done'
+        if sha256 == file.sha256:
+            file.status = FileStatus.ALREADY_DOWNLOADED
         else:
-            info['status'] = 'corrupted'
+            file.status = FileStatus.CORRUPTED
+
+        # TODO: overwrite
+        file.progress = 1
         update_root_progress()
-        print_root_info()
+        print_root_file()
         return
 
-    request = service.files().get_media(fileId=info['id'])
-    with open(info['path'], 'wb') as f:
+    # download the file
+    request = service.files().get_media(fileId=file.id)
+    with open(file_path, 'wb') as f:
         downloader = MediaIoBaseDownload(f, request)
         done = False
         while not done:
             status, done = downloader.next_chunk()
-            info['progress'] = status.progress()
+            file.progress = status.progress()
             update_root_progress()
-            print_root_info()
+            print_root_file()
 
-    info['status'] = 'downloaded'
-
-
-def download_file_recursive(info):
-    info['status'] = 'downloading'
-    if info['type'] == 'folder':
-        download_folder(info)
-    elif info['type'] == 'file':
-        download_file(info)
+    file.status = FileStatus.DOWNLOADED
 
 
-def main(file_id, save_path=''):
-    global root_info
-    root_info = get_file_info(file_id, save_path)
-    print_root_info()
-    if not root_info:
+def download_file_recursive(file: File, save_path: str) -> None:
+    """Download a file from Google Drive.
+
+    Args:
+        file (File): The file to download.
+        save_path (str, optional): The path to save the file. Defaults to ''.
+    """
+    file.status = FileStatus.DOWNLOADING
+    if file.type == FileType.FOLDER:
+        download_folder(file, save_path)
+    elif file.type == FileType.FILE:
+        download_file(file, save_path)
+
+
+def main():
+    global root_file, status, config
+
+    parse_args()
+
+    status = 'scanning'
+
+    root_file = File(config['file_id'])
+    scan_file(root_file)
+
+    if not root_file:
+        # TODO: no file found or no permission or ...
         return
 
-    download_file_recursive(root_info)
-    print_root_info()
+    status = 'downloading'
+    download_file_recursive(root_file, config['save_path'])
 
-def download_file_back(file_id, save_path=''):
-    creds = service_account.Credentials.from_service_account_file(
-        'key.json',
-        scopes=['https://www.googleapis.com/auth/drive']
-    )
+    status = 'done'
+    print_root_file()
+
+
+def scan_file(file: File):
+    """Scan a file from Google Drive.
+
+    Args:
+        file (File): The file to scan.
+    """
+
+    global service
 
     try:
-        service = build("drive", "v3", credentials=creds)
-        response = service.files().get(fileId=file_id).execute()
-        if response['mimeType'] == 'application/vnd.google-apps.folder':
-            folder_path = os.path.join(save_path, response['name'])
-            print(f"Downloading: {folder_path}", flush=True)
-            os.makedirs(folder_path, exist_ok=True)
-            page_token = None
-            while True:
-                response = (
-                    service.files()
-                    .list(
-                        q=f"'{file_id}' in parents",
-                        spaces="drive",
-                        fields="nextPageToken, files(id)",
-                        pageToken=page_token,
-                    )
-                    .execute()
-                )
-                items = response.get("files", [])
-                for e in items:
-                    download_file_back(e['id'], folder_path)
+        # get the file attributes
+        response = service.files().get(
+            fileId=file.id,
+            fields='id,name,mimeType,size,sha256Checksum',
+        ).execute()
 
-                page_token = response.get("nextPageToken", None)
-                if page_token is None:
-                    break
-            print(f"Downloaded: {folder_path}", flush=True)
-
+        # update the file attributes
+        file.id = response['id']
+        file.name = response['name']
+        if 'folder' in response['mimeType']:
+            file.type = FileType.FOLDER
+            file.size = 0
         else:
-            progress = 0
-            file_path = os.path.join(save_path, response['name'])
-            if os.path.exists(file_path):
-                print(f"File already exists: {file_path}", flush=True)
-                return
-            request = service.files().get_media(fileId=file_id)
-            fh = io.FileIO(file_path, mode='wb')
-            downloader = MediaIoBaseDownload(fh, request)
-            print(f"Downloading: {file_path} - {progress:.2f}%", end='', flush=True)
+            file.type = FileType.FILE
+            file.size = int(response['size'])
+        file.children = []
+        file.progress = 0
+        file.sha256 = response.get('sha256Checksum', '')
+        file.status = FileStatus.PENDING
 
-            done = False
-            while not done:
-                status, done = downloader.next_chunk()
-                progress = status.progress() * 100
-                print(f"\rDownloading: {file_path} - {progress:.2f}%", end='', flush=True)
-
-            print(f"\rDownloaded: {file_path}", flush=True)
-
-    except HttpError as error:
-        print(f"An error occurred: {error}", flush=True)
-
-
-def get_file_info(file_id, save_path=''):
-    info = {}
-    # architecture:
-    # - id: the id of the file
-    # - name: the name of the file
-    # - type: the type of the file (folder or file)
-    # - children: the children of the file (only for folders)
-    # - size: the size of the file
-    # - path: the path of the file
-    # - sha256: the sha256 of the file
-    # - progress: the download progress of the file (0 to 1)
-    # - status: the status of the file:
-    #   - pending: the file is pending download
-    #   - downloading: the file is downloading
-    #   - downloaded: the file was already downloaded
-    #   - done: the file was already downloaded and the sha256 was checked
-    #   - corrupted: the file was already downloaded but the sha256 was not the same
-
-    creds = service_account.Credentials.from_service_account_file(
-        'key.json',
-        scopes=['https://www.googleapis.com/auth/drive']
-    )
-
-    try:
-        service = build("drive", "v3", credentials=creds)
-        response = service.files().get(fileId=file_id, fields='id,name,mimeType,size,sha256Checksum').execute()
-
-        file_path = os.path.join(save_path, response['name'])
-
-        info = {
-            'id': response['id'],
-            'name': response['name'],
-            'type': 'folder' if 'folder' in response['mimeType'] else 'file',
-            'size': int(response.get('size', 0)),
-            'path': file_path,
-            'children': [],
-            'progress': 0,
-            'sha256': response.get('sha256Checksum', ''),
-            'status': 'pending',
-        }
-        if info['type'] == 'folder':
+        if file.type == FileType.FOLDER:
             page_token = None
             while True:
-                children_response = (
-                    service.files()
-                    .list(
-                        q=f"'{file_id}' in parents",
-                        spaces="drive",
-                        fields="nextPageToken, files(id)",
-                        pageToken=page_token,
-                    )
-                    .execute()
-                )
+                # get the children files
+                children_response = service.files().list(
+                    q=f"'{file.id}' in parents",
+                    spaces="drive",
+                    fields="nextPageToken, files(id)",
+                    pageToken=page_token,
+                ).execute()
                 children_files = children_response.get("files", [])
-                for f in children_files:
-                    info['children'].append(get_file_info(f['id'], file_path))
 
+                # scan the children files
+                for f in children_files:
+                    child_file = File(f['id'])  # pass by reference to update the root file as well
+                    file.children.append(child_file)
+                    scan_file(child_file)
+
+                # break if there are no more children batch
                 page_token = children_response.get("nextPageToken", None)
                 if page_token is None:
                     break
 
-            info['size'] = sum([child['size'] for child in info['children']])
+            # update the size of the folder
+            file.size = sum([child.size for child in file.children])
+
     except HttpError as error:
         print(f"An error occurred: {error}", flush=True)
 
-    return info
+    print_root_file()
 
 
-def print_root_info():
-    global root_info
+def print_root_file():
+    global root_file, status
+    if not root_file:
+        raise Exception('Root file is not set') # TODO: better error handling
     print('\x1b[0;0H\x1b[J', end='', flush=True)
-    print_info(root_info)
+    print(f"Status: {status}", flush=True)
+    print_file(root_file)
 
 
-def print_info(info, indent=''):
-    # └├│
-    text = indent + f"{info['name']} - {print_size(info['size'])} {print_progress(info['progress'])}"
+def print_file(file: File, indent=''):
+    """Print the file info.
+
+    Args:
+        file (File): The file to print.
+        indent (str, optional): The parent indetation. Defaults to ''.
+    """
+    # TODO: colors
+    text = indent + f"{file.name} - {file.formatted_size} {file.formatted_progress}"
     print(text, flush=True)
     if indent == '':
         child_indent = ''
     else:
         child_indent = indent[:-2] + ('  ' if indent[-2:] == '└ ' else '│ ')
-    if info['progress'] < 1:
-        for child_i, child in enumerate(info['children']):
-            child_indent_i = '└ ' if child_i == len(info['children']) - 1 else '├ '
-            print_info(child, child_indent + child_indent_i)
+    for child_i, child in enumerate(file.children):
+        child_indent_i = '└ ' if child_i == len(file.children) - 1 else '├ '
+        print_file(child, child_indent + child_indent_i)
 
 
-def print_size(size):
+def format_size(size: float) -> str:
+    """Format the size in bytes to a human readable format.
+
+    Args:
+        size (float): The size in bytes.
+
+    Returns:
+        str: The size in a human readable format.
+    """
     units = ['B', 'KB', 'MB', 'GB', 'TB']
     unit = 0
     while size >= 1024 and unit < len(units) - 1:
@@ -236,15 +313,22 @@ def print_size(size):
     return f"{size:.2f} {units[unit]}"
 
 
-def print_progress(progress):
-    # ━╾─
-    bar_length = 4
-    bar = '━' * int(bar_length * progress) + ('╾' if progress * bar_length % 1 >= 0.5 else '─') + '─' * bar_length
-    bar = bar[:bar_length]
+def format_progress(progress: float) -> str:
+    """Format the progress bar.
+
+    Args:
+        progress (float): The progress between 0 and 1.
+
+    Returns:
+        str: The progress bar.
+    """
+    # TODO: check progress is between 0 and 1: clip or exception?
+    bar = '━' * int(BAR_LENGTH * progress) + ('╾' if progress * BAR_LENGTH % 1 >= 0.5 else '─') + '─' * BAR_LENGTH
+    bar = bar[:BAR_LENGTH]
     return f"{bar} {100*progress:.2f}%"
 
 
 if __name__ == "__main__":
-    file_id = '1GwXP-KpWOxOenOxITTsURJZQ_1pkd4-j' # MEAD
-    save_path = '../AMIGO'
-    main(file_id)
+    # file_id = '1GwXP-KpWOxOenOxITTsURJZQ_1pkd4-j' # MEAD
+    # file_id = '15UjOIbGJ2NapRG5SD288Zb6nlnY9iTaj'  # test
+    main()
